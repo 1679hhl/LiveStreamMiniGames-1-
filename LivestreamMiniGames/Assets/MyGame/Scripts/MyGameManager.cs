6322ec551776ecb246e017e88a1ac911ec8daa9d
1679hhl <3298146326@qq.com>
2025-10-22 23:33:33 +0800

    1

--- LivestreamMiniGames/Assets/MyGame/Scripts/MyGameManager.cs
+++ LivestreamMiniGames/Assets/MyGame/Scripts/MyGameManager.cs
@@ -1,16 +1,25 @@
 using UnityEngine;
+using TMPro;
 
 public class MyGameManager : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public float RedEnergy;
+    public float BlueEnergy;
+
+    public TextMeshProUGUI redEnergyText;
+    public TextMeshProUGUI blueEnergyText;
+
+    public void RefreshText()
     {
-        
+        if (redEnergyText != null)
+        {
+            redEnergyText.text =  RedEnergy.ToString("F1");
+        }
+
+        if (blueEnergyText != null)
+        {
+            blueEnergyText.text = BlueEnergy.ToString("F1");
+        }
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
